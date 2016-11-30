7984806d4cf8f437e1992763d78ef931cb76c82d
yabesWirawan <yabes.wirawan@gmail.com>
2016-11-30 11:33:07 +0700

    add csv converter and download function

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ var cookieParser = require('cookie-parser')
 var sessions = require('express-session')
 var hbs = require('express-handlebars')
 var request = require('request')
+var fs = require('fs')
+var json2csv = require('json2csv')
+var mime = require('mime');
 
 var app = express()
 var router = express.Router()
@@ -35,6 +38,18 @@ app.set('view engine', '.hbs')
 
 var session
 
+var scrap = function (callback){
+    // Do something ...
+    request('https://qlapa.com/api/search?searchQuery='+req.param('search')+'&men=true&women=true&page=1&nonEtnik=false&etnik=false&priceMin=0&priceMax=99999999&sortMode=1&custom=false&child=false', function (err,resp,html){
+        // Do something ...
+        var json = JSON.parse(html)
+        if(!err && resp.statusCode == 200){
+            return callback(json)
+        }
+        // console.log(json)
+    })
+}
+
 // static folder setup
 app.use(express.static(path.join(__dirname, '/views')))
 app.use('/styles', express.static(path.join(__dirname, '/public/css/')))
@@ -43,15 +58,7 @@ app.use('/styles', express.static(path.join(__dirname, '/public/css/')))
 app.get('/', function(req, res){
     // Do Something...
     res.render('index', {
-        title: 'Qlapa Web Scrapper',
-    })
-})
-
-app.get('/about', function(req, res){
-    // Do Something...
-    res.render('about', {
-        title: 'About Us',
-        data: ['test1', 'test2', 'test3']
+        title: 'Qlapa Web Scraper',
     })
 })
 
@@ -64,11 +71,52 @@ app.get('/product/search', function(req, res){
         if(!err && resp.statusCode == 200){
             res.render('search', {
                 title: req.param('search'),
-                data: json.items
+                data: json.items,
             })
         }
         // console.log(json)
     })
+    
+})
+
+app.get('/convert', function(req, res){
+    // Do Something...
+    request('https://qlapa.com/api/search?searchQuery='+req.param('query')+'&men=true&women=true&page=1&nonEtnik=false&etnik=false&priceMin=0&priceMax=99999999&sortMode=1&custom=false&child=false', function (err,resp,html){
+        // Do something ...
+        var json = JSON.parse(html)
+        if(!err && resp.statusCode == 200){
+            json2csv({
+                data: json.items,
+                fields: ['itemId', 'title', 'shopName', 'minPrice', 'photoUrl', 'productDetailUrl', 'shopUrl']
+            }, function (err,csv){
+                // Do something ...
+                if (err) throw err
+                fs.writeFile('download.csv', csv, (err) => {
+                if (err) throw err;
+                    res.render('index',{
+                        title: 'Qlapa Web Scraper',
+                        message: "Your file has been sucessfully downloaded ..."
+                    });
+                });
+            })
+            
+        }
+        // console.log(json)
+    })
+})
+
+app.get('/download', function(req, res){
+    // Do Something...
+    var file = __dirname + '/download.csv';
+
+    var filename = path.basename(file);
+    var mimetype = mime.lookup(file);
+
+    res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+    res.setHeader('Content-type', mimetype);
+
+    var filestream = fs.createReadStream(file);
+    filestream.pipe(res);
 })
 
 // app.get('/product/search/:term', function(req, res){
